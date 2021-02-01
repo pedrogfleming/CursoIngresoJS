f1c58cb38c025d974a2b9bb3ea6de0be5e5ff079
pedrogfleming <pedrogfleming@gmail.com>
2021-02-01 15:27:00 -0300

    Ejercicios 8 y 9

--- 1-EntradaSalida/jsEntradaSalida-09.js
+++ 1-EntradaSalida/jsEntradaSalida-09.js
@@ -6,5 +6,25 @@ en el cuadro de texto "RESULTADO".*/
 function mostrarAumento()
 {
 
-	alert("ok");
-}
+		//Reservo espacio en memoria creando las varables
+		
+		let sueldoingresado;
+		let resultadofinal;
+		let porcentaje;
+		let aumento = 10;
+		//Cargo el valor con los datos de entrada y lo parseo para que se pueda operarar 
+		
+		sueldoingresado = parseInt (document.getElementById("txtIdSueldo").value);
+		
+		resultadofinal = parseInt (document.getElementById("txtIdResultado").value);
+
+		porcentaje = parseInt
+		
+		//Realizo las operaciónes , primero saco el porcentaje y después se lo sumo
+		porcentaje = sueldoingresado * aumento / 100;
+		resultadofinal = sueldoingresado + porcentaje;
+		
+		//muestro el resultado final en la caja "Resultado"
+		document.getElementById("txtIdResultado").value = resultadofinal;
+
+		}
--- 1-EntradaSalida/jsEntradaSalida-10.js
+++ 1-EntradaSalida/jsEntradaSalida-10.js
@@ -5,5 +5,28 @@ mostrar el importe con un Descuento del 25 %
 en el cuadro de texto "RESULTADO"*/
 function mostrarAumento()
 {
-	alert("ok");
-}
+
+	//Reservo espacio en memoria creando las varables
+	
+	let sueldoingresado;
+	let resultadofinal;
+	let porcentaje;
+	let descuento = 25;
+	//Cargo el valor con los datos de entrada y lo parseo para que se pueda operarar 
+	
+	sueldoingresado = parseInt (document.getElementById("txtIdImporte").value);
+	
+	resultadofinal = parseInt (document.getElementById("txtIdResultado").value);
+
+	porcentaje = parseInt
+	
+	//Realizo las operaciónes , primero saco el porcentaje y después se lo resto
+
+	porcentaje = sueldoingresado * descuento / 100;
+	resultadofinal = sueldoingresado - porcentaje;
+	
+	
+	//muestro el resultado final en la caja "Resultado"
+	document.getElementById("txtIdResultado").value = resultadofinal;
+
+	}
