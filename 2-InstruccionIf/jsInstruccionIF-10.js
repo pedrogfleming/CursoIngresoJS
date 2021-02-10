75a52f6e47fde971b7eea8d51d3da28e27791398
pedrogfleming <pedrogfleming@gmail.com>
2021-02-09 23:43:48 -0300

    Ejercicio IF 9,10 y tp 4

--- 2-InstruccionIf/jsInstruccionIF-10.js
+++ 2-InstruccionIf/jsInstruccionIF-10.js
@@ -1,5 +1,13 @@
 function mostrar()
 {
+
+/*Test
+notaAlumno = 9 EXCELENTE
+notaAlumno = 8 APROBÓ
+notaAlumno = 4 APROBÓ
+notaAlumno = 3 Vamos, la proxima se puede
+*/
+
 	//Genero el número RANDOM entre 1 y 10 
 
 	//Declaro la variable
@@ -11,6 +19,8 @@ function mostrar()
 	notaAlumno =Math.floor(Math.random() * 10);
 
 	//Parseo nota alumno
+	//Hardcodeo
+	//notaAlumno = 3;
 	
 	notaAlumno = parseInt(notaAlumno);
 	console.log(notaAlumno);
@@ -18,22 +28,21 @@ function mostrar()
 	//Invoco el if
 
 	if(notaAlumno < 4)
-		{
+	{
 		alert("Vamos, la proxima se puede")
-		}
+	}
 	
 	else
-		{
+	{
 			if(notaAlumno > 8)
-				{
-					alert("EXCELENTE");
-				}
+			{
+				alert("EXCELENTE");
+			}
 			else
-
-				{
-					alert("APROBÓ");
-				}
-		}
+			{
+				alert("APROBÓ");
+			}
+	}
 			
 
 			
--- 8-TPs/04-jsFerreteIluminacion.js
+++ 8-TPs/04-jsFerreteIluminacion.js
@@ -9,6 +9,115 @@ E.	Si el importe final con descuento suma más de $120  se debe sumar un 10% de 
 
  */
 function CalcularPrecio () 
-{
- 	
+{ 
+    //Declaro las variables
+    let cantidadLamparitas;
+    let montoCompra;
+    let marcaLamparita;
+    let descuentoMonto;
+    let porcentajeMonto;
+    let precioFinal;
+   //Usuario ingresa la cantidad de lamparitas que compró y la marca
+    cantidadLamparitas = document.getElementById("txtIdCantidad").value;
+    marcaLamparita = document.getElementById("Marca").value;    
+    //Parseo la cantidad de lamparitas ingresadas    
+    cantidadLamparitas =parseInt(cantidadLamparitas); 
+    
+    if(cantidadLamparitas > 5)//Invoco el if del descuento de 50%
+    {
+        porcentajeMonto = 0.50;
+        //precioFinal = montoCompra - (montoCompra * 0.5);
+    }
+    else
+    {
+        if(cantidadLamparitas == 5)//Si son 5 lamparitas
+        {
+            if(marcaLamparita == "ArgentinaLuz") //Si son marca Argentina luz, 40% de descuento
+            {
+                porcentajeMonto = 0.40;
+                //precioFinal = montoCompra - (montoCompra * 0.4);
+            }
+            else//Si son de cualquier otra marca, 30%
+            {
+                porcentajeMonto = 0.30;
+                //  precioFinal = montoCompra - (montoCompra * 0.3);
+            }           
+        }
+        else//Si no agarra ninguna promoción, se le cobra la tarifa standard
+        {
+            porcentaje = 1; 
+        }
+    }
+    montoCompra = cantidadLamparitas*35;
+    alert(montoCompra);   
+    alert(porcentajeMonto);
+    //descuentoMonto = montoCompra*porcentajeMonto;
+    //precioFinal = montoCompra-descuentoMonto;
+    document.getElementById("txtIdprecioDescuento").value = precioFinal+" pesos";
+}//FIN CODIGO
+
+
+
+//CODIGO VIEJO
+/*function CalcularPrecio () 
+{ 
+    //Declaro las variables
+    let cantidadLamparitas;
+    //let precioLamparitas;
+    let montoCompra;
+    let marcaLamparita;
+    let descuentoMonto;
+    let porcentajeMonto;
+    let precioFinal;
+   //Defino precio de las lamparitas
+   //precioLamparitas = 35;
+   //Usuario ingresa la cantidad de lamparitas que compró y la marca
+    cantidadLamparitas = document.getElementById("txtIdCantidad").value;
+    marcaLamparita = document.getElementById("Marca").value;    
+    //Parseo la cantidad de lamparitas ingresadas    
+    cantidadLamparitas =parseInt(cantidadLamparitas);
+ 
+    //precioFinal = precioLamparitas*cantidadLamparitas-descuentoMonto;
+    //precioFinal = parseInt(precioFinal);
+    //Multiplico precio por cantidad de lamparitas compradas y establezco una variable para reducir operaciones dentro de los if
+
+    
+    if(cantidadLamparitas > 5)//Invoco el if del descuento de 50%
+    {
+        porcentajeMonto = 0.50;
+        //descuentoMonto = montoCompra*0.5;
+        //precioFinal = montoCompra-descuentoMonto;
+       //document.getElementById("txtIdprecioDescuento").value = precioFinal+" pesos incluyendo un descuento de "+descuentoMonto+" pesos";
+    }
+    else
+    {
+        if(cantidadLamparitas == 5)//Si son 5 lamparitas
+        {
+            if(marcaLamparita == "ArgentinaLuz") //Si son marca Argentina luz, 40% de descuento
+            {
+                porcentajeMonto = 0.40;
+                //descuentoMonto = montoCompra*0.40;
+                //precioFinal = montoCompra-descuentoMonto;
+                //document.getElementById("txtIdprecioDescuento").value = precioFinal+" pesos incluyendo un descuento de "+descuentoMonto+" pesos";
+            }
+            else//Si son de cualquier otra marca, 30%
+            {
+                porcentajeMonto = 0.30;
+                //descuentoMonto = montoCompra*0.30;
+                //precioFinal = montoCompra-descuentoMonto;
+                //document.getElementById("txtIdprecioDescuento").value = precioFinal+" pesos incluyendo un descuento de "+descuentoMonto+" pesos";
+            }           
+        }
+        else//Si no agarra ninguna promoción, se le cobra la tarifa standard
+        {
+            porcentajeMonto = 1; 
+        }
+    }
+    montoCompra = cantidadLamparitas*35;
+    alert(montoCompra);   
+    alert(porcentajeMonto);
+    descuentoMonto = montoCompra*porcentajeMonto;
+    precioFinal = montoCompra-descuentoMonto;
+    document.getElementById("txtIdprecioDescuento").value = precioFinal+" pesos";
 }
+*/
